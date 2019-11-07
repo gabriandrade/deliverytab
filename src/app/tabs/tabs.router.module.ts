f001f948fc108df27f22718211fa8267950d88fd
GABRIEL MACHADO DE ANDRADE <gabriel.mandrade1@senacsp.edu.br>
2019-11-06 21:51:32 -0300

    1.2

--- src/app/tabs/tabs.router.module.ts
+++ src/app/tabs/tabs.router.module.ts
@@ -72,6 +72,10 @@ const routes: Routes = [
       {
         path: 'forma-pagamento',
         loadChildren: '../pedidos/form-pagamento/form-pagamento.module#FormPagamentoPageModule'
+      },
+      {
+        path: 'produtos/:key',
+        loadChildren: '../pedidos/lista-produto-pedido/lista-produto-pedido.module#ListaProdutoPedidoPageModule'
       }
     ]
   },
